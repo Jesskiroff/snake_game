fadf963b599871c492c9ded8eb5fc9368374e410
Jessica Kiroff <jesskiroff@gmail.com>
2026-01-22 17:42:03 +0200

    create main file and set up screen

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import Screen, Turtle, exitonclick
+
+
+
+timmy = Turtle()
+# timmy.shape = 
+
+screen = Screen()
+screen.setup(width=600, height = 600)
+screen.bgcolor("black")
+screen.title("My snake game")
+
+
+
+screen.exitonclick()
