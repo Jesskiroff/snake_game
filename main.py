c845eca9a799f1450aa8ab196962d5de601c3090
Jessica Kiroff <jesskiroff@gmail.com>
2026-01-22 17:59:45 +0200

    create snake using a for loop

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from turtle import Screen, Turtle, exitonclick
 
 
-
-# timmy = Turtle()
-# timmy.shape = 
-
 screen = Screen()
 screen.setup(width=600, height = 600)
 screen.bgcolor("black")
@@ -15,7 +11,7 @@ starting_positions = [(0,0), (-20,0), (-40,0)]
 
 for position in starting_positions:
     new_segment = Turtle("square")
-    new_segment.color=("white")
+    new_segment.color("white")
     new_segment.goto(position)
 
 
