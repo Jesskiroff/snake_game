66f8fa2d748c214db4220537f16f42fedc655a7b
Jessica Kiroff <jesskiroff@gmail.com>
2026-01-23 13:21:06 +0200

    create moving snake pieces via while loop

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ screen.setup(width=600, height = 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-
 starting_positions = [(0,0), (-20,0), (-40,0)]
 
 
@@ -17,7 +16,9 @@ segments = []
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
+    new_segment.penup()
     new_segment.goto(position)
+    segments.append(new_segment)
 
 
 game_is_on = True
