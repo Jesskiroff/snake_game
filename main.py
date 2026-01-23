61420acf711c0c7b3f8519a8b9f03ab8e29745e3
Jessica Kiroff <jesskiroff@gmail.com>
2026-01-23 13:23:51 +0200

    slow down the game using the time module to get a clear picture of how the code functions

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from turtle import Screen, Turtle, exitonclick
+from turtle import Screen, Turtle, exitonclick, time
 
 
 screen = Screen()
 screen.setup(width=600, height = 600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 starting_positions = [(0,0), (-20,0), (-40,0)]
 
@@ -21,10 +22,14 @@ for position in starting_positions:
     segments.append(new_segment)
 
 
+
+
 game_is_on = True
 while game_is_on:
     for seg in segments:
         seg.forward(20)
+        screen.update()
+        time.sleep(1)
 
 
 
