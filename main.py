e6b6da91423bccd34cfa9ebc1fc6d4183a962f0e
Jessica Kiroff <jesskiroff@gmail.com>
2026-01-23 12:33:10 +0200

    create a continuous snake moving while loop

--- main.py
+++ main.py
@@ -9,24 +9,22 @@ screen.title("My Snake Game")
 
 starting_positions = [(0,0), (-20,0), (-40,0)]
 
+
+segments = []
+#if you want something to continuously happen in a program, you usually use a while loop
+
+
 for position in starting_positions:
     new_segment = Turtle("square")
     new_segment.color("white")
     new_segment.goto(position)
 
 
-
-# segment_1 = Turtle("square")
-# segment_1.color("white")
-
-
-# segment_2 = Turtle("square")
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
 
 
-# segment_3 = Turtle("square")
-# segment_3.color("white")
-# segment_3.goto(-40, 0)
 
 screen.exitonclick()
