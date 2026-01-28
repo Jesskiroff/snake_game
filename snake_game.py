b8b9419389757363cfe7e9a75798f4dfdcc62089
Jessica Kiroff <jesskiroff@gmail.com>
2026-01-28 17:40:01 +0200

    initialize snake class file and add snake class

--- snake_game.py
+++ snake_game.py
@@ -21,19 +21,12 @@ for position in starting_positions:
     segments.append(segment)
 
 
-screen.update()
-
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     for seg in segments:
         seg.forward(20)
-        
-        
-
-
-
 
 
 screen.exitonclick()
